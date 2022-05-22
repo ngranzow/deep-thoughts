85f731458151a206d5a43bba4ac289ab89c065ef
ngranzow <nate.granzow@gmail.com>
2022-05-21 23:49:41 -0500

    finished initial react setup and linked server and client

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -14,6 +15,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
