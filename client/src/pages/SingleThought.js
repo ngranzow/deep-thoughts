810b5d247402bad807a02cd8c7b4b500e00b1bcb
ngranzow <nate.granzow@gmail.com>
2022-05-22 21:18:35 -0500

    added auth and mutations and updated queries, header, home, login, profile, signup, singlethought, and app

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -4,7 +4,7 @@ import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
 import ReactionList from '../components/ReactionList';
 
-const SingleThought = props => {
+const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
 
   const { loading, data } = useQuery(QUERY_THOUGHT, {
